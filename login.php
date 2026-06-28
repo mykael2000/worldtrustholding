2b748cdf398aee63f5e8ce991a2426c92b556348
Erameh Michael <eramehmichael2000@gmail.com>
2026-06-28 02:45:07 +0100

    Update login.php

--- login.php
+++ login.php
@@ -613,6 +613,19 @@ if (isset($_POST['login'])) {
     };
 </script>
 <script src="../cdn.gtranslate.net/widgets/latest/float.js" defer></script>
+<!-- Smartsupp Live Chat script -->
+<script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '6b200524a58ae82ef528db0457bd6b9a36efba7a';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript>Powered by <a href="https://www.smartsupp.com" target="_blank">Smartsupp</a></noscript>
+    
 </body>
 
 <!-- Mirrored from worldtrustholding.com/login by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 11 Oct 2025 21:44:55 GMT -->
